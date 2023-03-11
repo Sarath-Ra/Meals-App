0bb03a722880f25eb60cf258c766c24742ac8c88
SARATH RA <sarathra25@gmail.com>
2023-03-11 23:43:36 +0530

    "created pages and added navigation"

---
+++ lib/categories_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import './dummy_data.dart';
+import './category_item.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Meals Recipe'), backgroundColor: Theme.of(context).primaryColor),
+      body: GridView(
+        padding: EdgeInsets.all(25),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+              maxCrossAxisExtent: 200,
+              childAspectRatio: 3 / 2,
+              crossAxisSpacing: 20,
+              mainAxisSpacing: 20),
+          children: DUMMY_CATEGORIES.map((catData) {
+            return CategoryItem(
+              catData.id,
+              catData.title,
+              catData.color
+            );
+          }).toList()),
+    );
+  }
+}
